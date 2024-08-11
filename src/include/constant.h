aca65eb20ea2185b82dc079bfcb34bdfc4110db5
Fovir <fovir@disroot.org>
2024-08-11 21:10:32 +0800

    add Operations enum

--- src/include/constant.h
+++ src/include/constant.h
@@ -2,13 +2,17 @@
  * @Author: Fovir
  * @Email: fovir@disroot.org
  * @Date: 2024-08-11 21:04:56
- * @Last Modified by:   undefined
- * @Last Modified time: 2024-08-11 21:04:56
+ * @Last Modified by:   Fovir
+ * @Last Modified time: 2024-08-11 21:09:54
  * @Description: The file include constants to be used in this project.
  */
 
 #ifndef _CONSTANT_H_
 #define _CONSTANT_H_
 
+enum Operations
+{
+	ADD, REMOVE, LIST, RESET
+};
 
 #endif // !_CONSTANT_H_
