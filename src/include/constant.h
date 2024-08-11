e97e825db9d8d1c5bb103be842afddf2520d8f64
Fovir <fovir@disroot.org>
2024-08-11 22:09:14 +0800

    update constant.h

--- src/include/constant.h
+++ src/include/constant.h
@@ -3,16 +3,23 @@
  * @Email: fovir@disroot.org
  * @Date: 2024-08-11 21:04:56
  * @Last Modified by:   Fovir
- * @Last Modified time: 2024-08-11 21:11:47
+ * @Last Modified time: 2024-08-11 21:42:32
  * @Description: The file include constants to be used in this project.
  */
 
 #ifndef _CONSTANT_H_
 #define _CONSTANT_H_
 
+#define OPEARTION_NUMBER 4
+
 enum Operations
 {
 	ADD, REMOVE, LIST, RESET, ERROR
+};
+
+const char * OPERATIONS[OPEARTION_NUMBER] =
+{
+	"add","remove","list","reset"
 };
 
 #endif // !_CONSTANT_H_
