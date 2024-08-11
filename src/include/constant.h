1a4e009c282862bad0936db10bb3cdeaa32606d8
Fovir <fovir@disroot.org>
2024-08-11 21:08:42 +0800

    add constant.h

---
+++ src/include/constant.h
@@ -0,0 +1,14 @@
+/*
+ * @Author: Fovir
+ * @Email: fovir@disroot.org
+ * @Date: 2024-08-11 21:04:56
+ * @Last Modified by:   undefined
+ * @Last Modified time: 2024-08-11 21:04:56
+ * @Description: The file include constants to be used in this project.
+ */
+
+#ifndef _CONSTANT_H_
+#define _CONSTANT_H_
+
+
+#endif // !_CONSTANT_H_
