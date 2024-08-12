159f79f28edb45da9626cee8658bd8db378a4158
Fovir <fovir@disroot.org>
2024-08-12 17:49:09 +0800

    add add_new_event function

--- src/link_list.c
+++ src/link_list.c
@@ -1,10 +1,36 @@
 #include"include/link_list.h"
 #include<stdlib.h>
+#include<stdio.h>
 
 void initialize_link_list(LinkList * plist)
 {
-	plist->head = NULL;
-	plist->tail = NULL;
+	*plist = NULL;
 
 	return;
+}
+
+bool add_new_event(LinkList * plist, Event new_event)
+{
+	Node * pnew = (Node *) malloc(sizeof(Node));
+	Node * scan = *plist;
+
+	if (!pnew)
+	{
+		fprintf(stderr, "Can't allocate space.\n");
+		return false;
+	}
+
+	pnew->node_event = new_event;
+	pnew->next = NULL;
+
+	if (scan == NULL)	/* the link list is empty */
+		*plist = pnew;
+	else
+	{
+		while (scan->next)
+			scan = scan->next;
+		scan->next = pnew;
+	}
+
+	return true;
 }
