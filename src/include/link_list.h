301a199473c73c18f8f3051e970cbbb32c20b447
Fovir <fovir@disroot.org>
2024-08-12 21:14:57 +0800

    enable add_new_event to add event in time order

--- src/include/link_list.h
+++ src/include/link_list.h
@@ -3,7 +3,7 @@
  * @Email: fovir@disroot.org
  * @Date: 2024-08-12 13:35:33
  * @Last Modified by:   Fovir
- * @Last Modified time: 2024-08-12 17:44:34
+ * @Last Modified time: 2024-08-12 21:08:50
  * @Description: This file defines link list for event.
  */
 
@@ -26,10 +26,11 @@ typedef Node * LinkList;
 /// @brief The function is used to initialize link list, which set head and tail to NULL.
 void initialize_link_list(LinkList * plist);
 
-/// @brief The function is used to add new event to the link list.
+/// @brief The function is used to add new event to the link list. In time order.
 /// @param plist the pointer of the target link list.
 /// @param new_event the event to be added.
 /// @return If the function added successfully, it will return true. Otherwise, it returns false.
 bool add_new_event(LinkList * plist, Event new_event);
+
 
 #endif // !_LINK_LIST_H_
--- src/link_list.c
+++ src/link_list.c
@@ -13,6 +13,7 @@ bool add_new_event(LinkList * plist, Event new_event)
 {
 	Node * pnew = (Node *) malloc(sizeof(Node));
 	Node * scan = *plist;
+	Node * scan_next = scan->next;
 
 	if (!pnew)
 	{
@@ -27,9 +28,31 @@ bool add_new_event(LinkList * plist, Event new_event)
 		*plist = pnew;
 	else
 	{
-		while (scan->next)
+		while (scan_next->next
+			&& compare_events(&scan_next->node_event,
+				&pnew->node_event) == FIRST_EARLIER_THAN_SECOND)
+		{
 			scan = scan->next;
+			scan_next = scan_next->next;
+		}
+
+		// time conflict
+		if (compare_events(&scan_next->node_event,
+			&pnew->node_event) == FIRST_AND_SECOND_ARE_THE_SAME)
+		{
+			fprintf(stderr, "The time has been occupied by event: %s\n",
+				scan_next->node_event.title);
+			free(pnew);	/* delete the item */
+
+			return false;
+		}
+		/*
+		now the link list looks like:
+		scan	pnew	scan_next
+		*/
+
 		scan->next = pnew;
+		pnew->next = scan_next;
 	}
 
 	return true;
