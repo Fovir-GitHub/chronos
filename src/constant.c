680cfbf5236b5e8744d585fc0ed68a9569e69420
Fovir <fovir@disroot.org>
2024-08-12 14:39:07 +0800

    move g_operation to constant.c

---
+++ src/constant.c
@@ -0,0 +1,6 @@
+#include"include/constant.h"
+
+const char * g_operations[OPEARTION_NUMBER] =
+{
+	"add","remove","list","reset"
+};
--- src/include/constant.h
+++ src/include/constant.h
@@ -3,7 +3,7 @@
  * @Email: fovir@disroot.org
  * @Date: 2024-08-11 21:04:56
  * @Last Modified by:   Fovir
- * @Last Modified time: 2024-08-12 12:41:04
+ * @Last Modified time: 2024-08-12 14:35:31
  * @Description: The file include constants to be used in this project.
  */
 
@@ -13,15 +13,15 @@
 #define OPEARTION_NUMBER 4
 #define TITLE_LENGTH 81
 #define DETAIL_LENGTH 129
+#define DATE_LENGTH 11
+
+#define ERROR -1
 
 enum Operations
 {
-	ADD, REMOVE, LIST, RESET, ERROR
+	ADD, REMOVE, LIST, RESET
 };
 
-const char * g_operations[OPEARTION_NUMBER] =
-{
-	"add","remove","list","reset"
-};
+extern const char * g_operations[OPEARTION_NUMBER];
 
 #endif // !_CONSTANT_H_
