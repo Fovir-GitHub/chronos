2e35e70e400459e9dba32c476358a17daa4708c3
Fovir <fovir@disroot.org>
2024-08-13 18:06:22 +0800

    add show_link_list function update the logical of insert node to a link list.

--- src/link_list.c
+++ src/link_list.c
@@ -13,7 +13,6 @@ bool add_new_event(LinkList * plist, Event new_event)
 {
 	Node * pnew = (Node *) malloc(sizeof(Node));
 	Node * scan = *plist;
-	Node * scan_next = scan->next;
 
 	if (!pnew)
 	{
@@ -28,32 +27,66 @@ bool add_new_event(LinkList * plist, Event new_event)
 		*plist = pnew;
 	else
 	{
-		while (scan_next->next
-			&& compare_events(&scan_next->node_event,
+		Node * previous = NULL;
+		while (scan != NULL
+			&& compare_events(&scan->node_event,
 				&pnew->node_event) == FIRST_EARLIER_THAN_SECOND)
 		{
+			previous = scan;
 			scan = scan->next;
-			scan_next = scan_next->next;
 		}
 
 		// time conflict
-		if (compare_events(&scan_next->node_event,
+		if (compare_events(&previous->node_event,
 			&pnew->node_event) == FIRST_AND_SECOND_ARE_THE_SAME)
 		{
 			fprintf(stderr, "The time has been occupied by event: %s\n",
-				scan_next->node_event.title);
+				scan->node_event.title);
 			free(pnew);	/* delete the item */
 
 			return false;
 		}
-		/*
-		now the link list looks like:
-		scan	pnew	scan_next
-		*/
 
-		scan->next = pnew;
-		pnew->next = scan_next;
+		// insert to the head
+		if (!previous)
+		{
+			pnew->next = *plist;
+			*plist = pnew;
+		}
+		else
+		{
+			previous->next = pnew;
+			pnew->next = scan;
+		}
 	}
 
 	return true;
+}
+
+void show_link_list(const LinkList * plist)
+{
+	Node * scan = *plist;
+
+	while (scan)
+	{
+		printf("%s\n", scan->node_event.title);
+		printf("    From %d-%02d-%02d %02d:%02d to %d-%02d-%02d %02d:%02d\n",
+			scan->node_event.start_time->year,
+			scan->node_event.start_time->month,
+			scan->node_event.start_time->day,
+			scan->node_event.start_time->hour,
+			scan->node_event.start_time->minute,
+			scan->node_event.due_time->year,
+			scan->node_event.due_time->month,
+			scan->node_event.due_time->day,
+			scan->node_event.due_time->hour,
+			scan->node_event.due_time->minute);
+		printf("    UID = %3d, Status: %s\n", scan->node_event.uid,
+			(scan->node_event.status ? "Finished" : "Wait to finish"));
+		printf("    Details: %s\n", scan->node_event.detail);
+
+		scan = scan->next;
+	}
+
+	return;
 }
