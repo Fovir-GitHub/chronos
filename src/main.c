7854435a22651a5a8fa682eab861eba111dd5a6a
Fovir <fovir@disroot.org>
2024-08-13 18:07:29 +0800

    test show_link_list function

--- src/main.c
+++ src/main.c
@@ -2,10 +2,15 @@
 #include"include/choose_operation.h"
 #include"include/schedule_event.h"
 #include"include/link_list.h"
+#include"include/json_operation.h"
 
 int main(int argc, char * argv[])
 {
 	LinkList schedule;
+	initialize_link_list(&schedule);
+	read_event_from_json_file(&schedule);
+
+	show_link_list(&schedule);
 
 	return 0;
 }
