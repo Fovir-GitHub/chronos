52161f09e4304696208d47e49f94b0812ad41d4e
Fovir <fovir@disroot.org>
2024-08-13 22:14:27 +0800

    test add event function

--- src/main.c
+++ src/main.c
@@ -3,12 +3,19 @@
 #include"include/schedule_event.h"
 #include"include/link_list.h"
 #include"include/json_operation.h"
+#include"include/global_function.h"
 
 int main(int argc, char * argv[])
 {
+	global_initialize();
+
 	LinkList schedule;
 	initialize_link_list(&schedule);
 	read_event_from_json_file(&schedule);
+
+	int choice = get_operation(argv[1]);
+	if (choice == ADD)
+		add_event_from_user_input(&schedule);
 
 	show_link_list(&schedule);
 
