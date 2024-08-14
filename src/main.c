f695b94d21dd69297ec3bb3cbae10ee3b8ff54f1
Fovir <fovir@disroot.org>
2024-08-14 16:08:24 +0800

    test remove operation function

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
 #include"include/choose_operation.h"
 #include"include/schedule_event.h"
 #include"include/link_list.h"
@@ -14,8 +17,43 @@ int main(int argc, char * argv[])
 	read_event_from_json_file(&schedule);
 
 	int choice = get_operation(argv[1]);
-	if (choice == ADD)
-		add_event_from_user_input(&schedule);
+
+	switch (choice)
+	{
+	case ADD:
+		add_operation(&schedule);
+		break;
+	case LIST:
+		show_link_list(&schedule);
+		break;
+	case REMOVE:
+		if (argc == 3)	/* input: chronos remove uid */
+		{
+			int unique_id = 0;
+			int length = strlen(argv[2]);
+			for (int i = 0; i < length; i++)
+			{
+				if (!isdigit(argv[2][i]))	/* there is non-digit */
+				{
+					fprintf(stderr, "Please enter right uid.");
+					exit(EXIT_FAILURE);
+				}
+				unique_id = unique_id * 10 + character2integer(argv[2][i]);
+			}
+
+			if (unique_id < 0 || unique_id >= UID_MAX_NUMBER)
+			{
+				fprintf(stderr, "The uid ranging from 0 - %d",
+					UID_MAX_NUMBER - 1);
+				exit(EXIT_FAILURE);
+			}
+
+			remove_event(&schedule, unique_id);
+		}
+		else
+			remove_operation(&schedule);
+		break;
+	}
 
 	show_link_list(&schedule);
 
